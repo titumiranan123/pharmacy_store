567c2a89c17c5b255c9c99fcadb97250c720e09b
MD Titumir Anan <titumiranan.gtc@gmail.com>
2024-03-16 12:48:55 +0600

    add complete

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "../../components/homecomponents/Header";
 import Categorysection from "../../components/homecomponents/Categorysection";
-
+import Arrivals from "../../components/homecomponents/Arrivals";
+import add from "../../assets/add/ad.jpg";
 const Home: React.FC = () => {
   return (
     <div className="max-w-[1240px] mx-auto">
       <Header />
       <Categorysection />
+      <Arrivals />
+      <div className="add mt-40 mb-32">
+        <a href="">
+          <img src={add} alt="" />
+        </a>
+      </div>
     </div>
   );
 };
